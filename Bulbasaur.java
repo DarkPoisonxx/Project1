8e82bf8f7ce095ccdbf0ae5cd9a477a0f2e93f4e
DarkPoisonxx <60245132+DarkPoisonxx@users.noreply.github.com>
2024-03-04 14:31:29 -0500

    reset

--- Bulbasaur.java
+++ Bulbasaur.java
@@ -1,17 +1,4 @@
 public class bulbasaur extends Pokemon{
 
-    public bulbasaur(){
-        setHp(70);
-    }
-
-    public void leechSeed(Pokemon target){
-
-        int currentHp = target.getHp();
-        int resultingHp = currentHp - 20;
-
-        int thisCurrentHp = this.getHp();
-        this.setHp(resultingHp);
-
-        target.setHp(resultingHp);
-    }
+    
 }
